702a92217e9df87025edaa1f38a5d7f3f288e51f
Emile Décosterd <emiledecosterd@macbook-pro-de-emile-2.fritz.box>
2016-07-31 17:11:31 +0200

    Initial Commit

---
+++ Reachability/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reachability
+//
+//  Created by Emile Décosterd on 31.07.16.
+//  Copyright © 2016 Emile Décosterd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
